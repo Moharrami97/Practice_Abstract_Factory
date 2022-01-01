ef00cd9f795ec8fcf774833255b2b7353bcbf9b8
Narges <narges.moharrami97@gmail.com>
2022-01-01 03:59:51 -0800

    start commit

---
+++ main.py
@@ -0,0 +1,43 @@
+class Rugs:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class DetailRugs:
+    pass
+
+
+class PriceRugs:
+    pass
+
+
+class Mobile:
+    def __init__(self, name, model, price):
+        self.name = name
+        self.model = model
+        self.price = price
+
+
+class DetailMobile:
+    pass
+
+
+class PriceMobile:
+    pass
+
+
+class GiftCart:
+    def __init__(self, company, min_price, max_price):
+        self.company = company
+        self.min_price = min_price
+        self.max_price = max_price
+
+
+class DetailGiftCart:
+    pass
+
+
+class PriceGiftCart:
+    pass
+
