d3d03d6d1d1f84e1433aa7662babe036181f2d4c
Narges <narges.moharrami97@gmail.com>
2022-01-01 05:51:21 -0800

    complete abstract_factory

--- main.py
+++ main.py
@@ -1,43 +1,120 @@
-class Rugs:
+from abc import ABC, abstractmethod
+
+
+class ProductBase(ABC):
+    @abstractmethod
+    def detail(self):
+        pass
+
+    @abstractmethod
+    def price(self):
+        pass
+
+
+class ProductShow(ABC):
+    @abstractmethod
+    def show(self):
+        pass
+
+
+class Rugs(ProductBase):
     def __init__(self, name, price):
         self.name = name
-        self.price = price
+        self._price = price
+
+    def detail(self):
+        return DetailRugs(self)
+
+    def price(self):
+        return PriceRugs(self)
 
 
-class DetailRugs:
-    pass
+class DetailRugs(ProductShow):
+    def __init__(self, rugs):
+        self.rugs = rugs
+
+    def show(self):
+        return f"name of the rug: {self.rugs.name}"
 
 
-class PriceRugs:
-    pass
+class PriceRugs(ProductShow):
+    def __init__(self, rugs):
+        self.rugs = rugs
+
+    def show(self):
+        return f"price of the rug: {self.rugs._price}"
 
 
-class Mobile:
+class Mobile(ProductBase):
     def __init__(self, name, model, price):
         self.name = name
         self.model = model
-        self.price = price
+        self._price = price
+
+    def detail(self):
+        return DetailMobile(self)
+
+    def price(self):
+        return PriceMobile(self)
 
 
-class DetailMobile:
-    pass
+class DetailMobile(ProductShow):
+    def __init__(self, mobile):
+        self.mobile = mobile
+
+    def show(self):
+        return f"model of the mobile: {self.mobile.model}"
 
 
-class PriceMobile:
-    pass
+class PriceMobile(ProductShow):
+    def __init__(self, mobile):
+        self.mobile = mobile
+
+    def show(self):
+        return f"price of the mobile: {self.mobile._price} "
 
 
-class GiftCart:
+class GiftCart(ProductBase):
     def __init__(self, company, min_price, max_price):
         self.company = company
         self.min_price = min_price
         self.max_price = max_price
 
+    def detail(self):
+        return DetailGiftCart(self)
 
-class DetailGiftCart:
-    pass
+    def price(self):
+        return PriceGiftCart(self)
 
 
-class PriceGiftCart:
-    pass
+class DetailGiftCart(ProductShow):
+    def __init__(self, cart):
+        self.cart = cart
 
+    def show(self):
+        return f"company of the giftcart: {self.cart.company}"
+
+
+class PriceGiftCart(ProductShow):
+    def __init__(self, cart):
+        self.cart = cart
+
+    def show(self):
+        return f"minimum price of the GiftCart: {self.cart.min_price}" \
+               f"maximum price of the GiftCart: {self.cart.max_price}"
+
+
+if __name__ == "__main__":
+    r1 = Rugs("persian rugs", 320)
+    r2 = Rugs("nain rugs", 150)
+
+    m1 = Mobile("samsung", "A30", 250)
+    m2 = Mobile("Apple", "A2275", 1000)
+
+    g1 = GiftCart("Google", 20, 60)
+    g2 = GiftCart("Apple", 30, 70)
+
+    products = [r1, r2, m1, m2, g1, g2]
+
+    for product in products:
+        print(f"{product.detail().show()} ----> {product.price().show()}")
